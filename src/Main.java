47bc24b403b1b3aa4afb901423f46515f2386226
17Ashbringer76 <77210553+17Ashbringer76@users.noreply.github.com>
2021-02-09 22:43:28 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 4522;
+        int spentRubles = 20;
+        int bonusMile = ticketPrice / spentRubles;
+        System.out.println(bonusMile);
+    }
+}
